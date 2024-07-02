f4e980eb874e6c90cfe05f7c360b2feb59ad0e59
Hannana <105469888+hancrysta1@users.noreply.github.com>
2024-07-02 20:21:43 +0900

    Update 정수 내림차순으로 배치하기.java
    + 오답노트

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -8,5 +8,6 @@ class Solution {
             con+=arr[arr.length-1-i];
         }
         return Long.parseLong(con);
+//런타임에러 : 리턴 값 Integer로 했을 때 났다. long 자료형이 int보다 처리할 수 있는 범위 큼. Java에서 Integer의 범위는 -2^31에서 2^31-1
     }
 }
