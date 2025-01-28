099b885960c502ddf8e76ef80e0da6fd3b9237ce
SooJeong Han <105469888+hancrysta1@users.noreply.github.com>
2025-01-28 09:11:49 +0900

    [Gold V] Title: ABCDE, Time: 172 ms, Memory: 22800 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,29 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean flag;
-    static List<Integer>[] graph;//갯수가 정해진 리스트들이므로 배열 사용하기
     static boolean[] visited;
+    static ArrayList<Integer>[] graph;
+    static boolean flag;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        if (N <= 0 || M < 0) {
-            System.out.println(0);
-            return;
+        visited = new boolean[N];
+        graph = new ArrayList[N];//N개 공간 만들겠다 -> N 초기화 필요
+        for(int i=0;i<N;i++){
+            graph[i] = new ArrayList<>();
         }
 
-        visited = new boolean[N];
-        graph = new ArrayList[N];//N개의 연결리스트
-        for(int i=0;i<N;i++){
-            graph[i] = new ArrayList<>();//공간 만들기
-        }
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -31,28 +27,28 @@ public class Main {
             graph[a].add(b);
             graph[b].add(a);
         }
+
         for(int i=0;i<N;i++){
-            if(flag) break;
-            visited = new boolean[N];
             DFS(i,1);
+            if(flag) break;//하나라도 나오면 반복문 중단
         }
         System.out.println(flag?1:0);
     }
 
-    public static void DFS(int start,int depth){
-        if (flag) return;
-        if(depth==5){
+    private static void DFS(int startNode, int depth) {
+        if(depth==5||flag){
             flag=true;
             return;
         }
-        visited[start]=true;
-        for(int x:graph[start]){
+        visited[startNode]=true;
+
+        for(int x:graph[startNode]){
             if(!visited[x]){
                 DFS(x,depth+1);
             }
         }
-        visited[start]=false;
+        
+        visited[startNode]=false;
     }
-
 }
 
