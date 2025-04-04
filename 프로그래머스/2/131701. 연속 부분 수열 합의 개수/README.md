be98d80a670d9c3bc4ff676cb8ba0e382bf0484d
SooJeong Han <105469888+hancrysta1@users.noreply.github.com>
2025-04-05 01:00:37 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 84.04 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 123 MB, 시간: 106.35 ms
+메모리: 114 MB, 시간: 84.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 03일 05:12:36
+2025년 04월 05일 01:00:33
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,25 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        int[] combined = new int[elements.length*2];
-        for(int i=0;i<elements.length;i++){
-            combined[i]=elements[i];
-            combined[elements.length+i]=elements[i];
+        int len = elements.length;
+        int[] elementsDouble = new int[len*2];
+        
+        for(int i=0;i<len;i++){
+            elementsDouble[i]=elements[i];
+            elementsDouble[i+len]=elements[i];
         }
         Set<Integer> sums = new HashSet<>();
-        for(int i=1;i<=elements.length;i++){
-            int sum = 0;
-            for(int j=0;j<i;j++){
-                sum+=combined[j];
+        for(int i=1;i<=len;i++){
+            int sum =0;
+            
+            for(int j=0;j<i;j++){//윈도우 내부 탐색
+                sum+=elementsDouble[j];
+                
+            }
+            sums.add(sum);
+            for(int x=1;x<len;x++){
+                sum = sum-elementsDouble[x-1]+elementsDouble[x+i-1];
                 sums.add(sum);
             }
-            for(int w=1;w<elements.length;w++){
-                sum=sum-combined[w-1]+combined[w+i-1];
-                sums.add(sum);
-            }
+            
         }
-        
         return sums.size();
     }
 }
