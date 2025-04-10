bb8e7f257e6a5a47ba3f7d98677618e0437d3a47
SooJeong Han <105469888+hancrysta1@users.noreply.github.com>
2025-04-10 19:10:14 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.08 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<k;i++){
+            sb.append(my_string);
+        }
+        return String.valueOf(sb);
+    }
+}
